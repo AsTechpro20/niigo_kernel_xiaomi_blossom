5a1c28104c2d1a5602ad63098a9959d43ba04e8e
Jules Irenge <jbi.octave@gmail.com>
2020-04-06 20:08:30 -0700

    BACKPORT: mm/zsmalloc: add missing annotation for unpin_tag()
    Sparse reports a warning at unpin_tag()()
    
    warning: context imbalance in unpin_tag() - unexpected unlock
    
    The root cause is the missing annotation at unpin_tag()
    Add the missing __releases(bitlock) annotation
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Link: http://lkml.kernel.org/r/20200214204741.94112-14-jbi.octave@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -924,7 +924,7 @@ static void pin_tag(unsigned long handle)
 	bit_spin_lock(HANDLE_PIN_BIT, (unsigned long *)handle);
 }
 
-static void unpin_tag(unsigned long handle)
+static void unpin_tag(unsigned long handle) __releases(bitlock)
 {
 	bit_spin_unlock(HANDLE_PIN_BIT, (unsigned long *)handle);
 }
